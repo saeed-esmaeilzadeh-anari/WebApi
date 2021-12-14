e86972ac4044d9a8eedba7c94d025fa455df2d82
saeed-esmaeilzadeh-anari <49111477+saeed-esmaeilzadeh-anari@users.noreply.github.com>
2021-12-14 18:57:04 +0330

    ss

--- WebApplication1/Models/RecipeDetails.cs
+++ WebApplication1/Models/RecipeDetails.cs
@@ -1,6 +1,21 @@
-﻿namespace WebApplication1.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
 {
   public class RecipeDetails
   {
+    [Key]
+    public int Id { get; set; }
+    
+    [Column(TypeName = "nvarchar(100)")]
+    public string Name { get; set; }
+    
+    [Column(TypeName = "nvarchar(100)")]
+    public string Description { get; set; }
+    
+    [Column(TypeName = "varchar(max)")]
+    public string ImgPath { get; set; }
+    
   }
 }
